51f5a0027f1e488587297f787cbed6d2da33bfb7
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-10-20 22:58:27 -0700

    set up boilerplate

---
+++ localguides/src/redux/reducers/index.js
@@ -0,0 +1,11 @@
+// REDUX
+import { combineReducers } from "redux";
+
+// REDUCERS
+import { guideReducer } from "./guide-reducers";
+
+// -- ** START CODE ** -- //
+export const rootReducer = combineReducers({
+  // guide: guideReducer
+  guideReducer
+});
