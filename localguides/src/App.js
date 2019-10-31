e4ee647c65fed0dea384f1ed3860eb873c637cee
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-10-31 14:04:15 -0700

    Set up login/register

--- localguides/src/App.js
+++ localguides/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
-import "./App.css";
-// Components
+// COMPONENTS
 import Dashboard from "./components/Guide/Dashboard/Dashboard";
+import G_Form from "./components/Guide/SignUp/G_Form";
+import Login from "./components/Guide/LogIn/Login";
+
+// ROUTES
+import { Route } from "react-router-dom";
+import PrivateRoute from "./utils/PrivateRoute";
+
+// STYLES
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       LOCAL GUIDES
       <Dashboard />
+      <G_Form />
+      <Login />
     </div>
   );
 }
---
+++ localguides/src/components/Guide/LogIn/Login.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { Form, Icon, Input, Button } from "antd";
+
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
+
+const Login = props => {
+  const [user, setUser] = useState({ username: "", password: "" });
+
+  // useEffect(() => {
+  //   if (token) {
+  //     history.push("/trips");
+  //   }
+  // }, [history, token]);
+
+  const handleChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.form.validateFields((err, values) => {
+      if (user.username && user.password) {
+        console.log("Received values of form: ", values);
+        setUser({ username: "", password: "" });
+      }
+    });
+  };
+
+  const {
+    getFieldDecorator,
+    getFieldsError,
+    getFieldError,
+    isFieldTouched
+  } = props.form;
+
+  const usernameError = isFieldTouched("username") && getFieldError("username");
+  const passwordError = isFieldTouched("password") && getFieldError("password");
+
+  return (
+    <div>
+      <Form layout="inline" onSubmit={handleSubmit}>
+        <Form.Item
+          validateStatus={usernameError ? "error" : ""}
+          help={usernameError || ""}
+        >
+          {getFieldDecorator("username", {
+            rules: [{ required: true, message: "Please input your username!" }]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Username"
+            />
+          )}
+        </Form.Item>
+        <Form.Item
+          validateStatus={passwordError ? "error" : ""}
+          help={passwordError || ""}
+        >
+          {getFieldDecorator("password", {
+            rules: [{ required: true, message: "Please input your Password!" }]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Password"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={hasErrors(getFieldsError())}
+          >
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    token: state.token
+  };
+};
+
+const WrappedHorizontalLoginForm = Form.create({ name: "horizontal_login" })(
+  Login
+);
+
+export default connect(
+  mapStateToProps,
+  {}
+)(WrappedHorizontalLoginForm);
