b6b561ed86b2cb34d77bde15d5a74c1ea4fb25dd
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-12-29 00:05:39 -0800

    set up nav bar

--- localguides/src/App.js
+++ localguides/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 // COMPONENTS
-import Dashboard from "./components/Guide/Dashboard/Dashboard";
-import G_Form from "./components/Guide/SignUp/G_Form";
-import Login from "./components/Guide/LogIn/Login";
+import HomePage from "./components/HomePage/HomePage";
+import Dashboard from "./components/Dashboard/Dashboard";
+import G_Form from "./components/SignUp/G_Form";
+import Login from "./components/LogIn/Login";
+import Navbar from "./components/Navbar/Navbar";
 
 // ROUTES
 import { Route } from "react-router-dom";
@@ -14,7 +16,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      LOCAL GUIDES
+      <Navbar />
+      <Route exact path="/home" component={HomePage} />
       <Route exact path="/register" component={G_Form} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/dashboard" component={Dashboard} />
