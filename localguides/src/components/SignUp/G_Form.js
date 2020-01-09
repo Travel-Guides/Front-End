cdfaf7590935ec71e5c8f9195c503fa66634b0af
Darren J Carrillo <darrenjcarrillo@gmail.com>
2020-01-08 16:29:37 -0800

    Register page styling complete

--- localguides/src/components/SignUp/G_Form.js
+++ localguides/src/components/SignUp/G_Form.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // REDUX
 import { connect } from "react-redux";
 // Antd
@@ -17,6 +17,32 @@ const G_Form = props => {
       }
     });
   };
+
+  const [isTourist, setIsTourist] = useState(true);
+
+  const [confirmation, setConfirmation] = useState(false);
+
+  // const register = e => {
+  //   e.preventDefault();
+
+  //   let creds = {
+  //     email: credentials.email,
+  //     username: credentials.username,
+  //     password: credentials.password
+  //   };
+
+  //   props.register(creds, isTourist);
+  //   setConfirmation(true);
+  // };
+
+  const toggleConfirm = () => {
+    setConfirmation(!confirmation);
+  };
+
+  const handleRadio = e => {
+    setIsTourist(!isTourist);
+  };
+
   const { getFieldDecorator } = props.form;
 
   const formItemLayout = {
@@ -45,14 +71,37 @@ const G_Form = props => {
   return (
     <div className="sign-up-container">
       <Form {...formItemLayout} onSubmit={handleSubmit}>
-        <h2 className="sign-up">Sign Up</h2>
+        <h2 className="sign-up">Are you a...</h2>
+
+        <div className="radio-buttons">
+          <label>
+            <div>
+              {isTourist ? (
+                <div className="checked-radio" onClick={handleRadio}></div>
+              ) : (
+                <div className="radio" onClick={handleRadio}></div>
+              )}
+              Tourist
+            </div>
+          </label>
+          <label>
+            <div>
+              {!isTourist ? (
+                <div className="checked-radio" onClick={handleRadio}></div>
+              ) : (
+                <div className="radio" onClick={handleRadio}></div>
+              )}
+              Guide
+            </div>
+          </label>
+        </div>
 
         <Form.Item label="First Name">
           {getFieldDecorator("firstName", {
             rules: [
               {
                 required: true,
-                message: "Please enter your full name!"
+                message: "Please enter your first name!"
               }
             ]
           })(<Input name="firstName" />)}
@@ -93,7 +142,7 @@ const G_Form = props => {
 
         <Form.Item {...tailFormItemLayout}>
           <Button type="primary" htmlType="submit" className="register-button">
-            Register
+            Sign Up
           </Button>
         </Form.Item>
       </Form>
